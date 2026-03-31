034a02f5fc933680152982b904d9b8da45480ad4
Ashish2343 <a98126959@gmail.com>
2026-03-31 12:58:13 +0530

    upgrage

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,7 +1,7 @@
 import { io, Socket } from "socket.io-client";
 
 let socket: Socket | null = null;
-
+// 
 const SOCKET_SERVER_URL = process.env.NEXT_PUBLIC_SOCKET_SERVER_URL || "https://meridian-backend-test-1.onrender.com/";
 
 export const getSocket = () => {
