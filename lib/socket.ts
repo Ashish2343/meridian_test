4cd36d2ae8e0f332ba0186de81a68e60402331f4
Ashish2343 <a98126959@gmail.com>
2026-03-31 12:12:06 +0530

    change socket-server.js

--- lib/socket.ts
+++ lib/socket.ts
@@ -2,9 +2,11 @@ import { io, Socket } from "socket.io-client";
 
 let socket: Socket | null = null;
 
+const SOCKET_SERVER_URL = process.env.NEXT_PUBLIC_SOCKET_SERVER_URL || "http://localhost:3001";
+
 export const getSocket = () => {
   if (!socket) {
-    socket = io("http://localhost:3001", {
+    socket = io(SOCKET_SERVER_URL, {
       autoConnect: false,
       transports: ["websocket"], // optional but faster
     });
